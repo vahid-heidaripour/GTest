80050b85ac9a1c77eabf060f30893fb3e1638c7d
Vahid <v.heidaripour@gmail.com>
2019-03-13 17:52:51 +0100

    test.cpp update

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -2,5 +2,31 @@
 // Created by Vahid Heidaripour on 2019-03-13.
 //
 
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "ClassTest.h"
 
+using testing::Eq;
+
+namespace
+{
+    class ClassDeclaration : public testing::Test
+    {
+        ClassTest obj;
+        ClassDeclaration()
+        {
+            obj;
+        }
+    };
+}
+
+TEST_F(ClassDeclaration, test1)
+{
+    ASSERT_EQ("", "");
+}
+
+TEST_F(ClassDeclaration, test2)
+{
+    ASSERT_EQ("", "1");
+}
 
